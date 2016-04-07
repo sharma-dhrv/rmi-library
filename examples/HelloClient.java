dec7c046b487e53b708f49c482872ada895ef65f
Karthikeyan Vasuki Balasubramaniam <kvasukib@ucsd.edu>
2016-04-07 10:35:05 -0700

    Adding Examples

---
+++ examples/HelloClient.java
@@ -0,0 +1,27 @@
+import java.rmi.*;
+
+class HelloClient
+{
+  // You'll want to change this to match your own host
+  private static final String HelloServerURL = "rmi://localhost/hello";
+
+
+  // This takes one command line argument: A person's first name
+  public static void main (String []args)
+  {
+    try 
+    {
+      System.setSecurityManager (new RMISecurityManager());
+      HelloInterface hello = (HelloInterface) Naming.lookup(HelloServerURL);
+
+      String theGreeting = hello.sayHello (args[0]);
+
+      System.out.println (theGreeting);
+    }
+    catch (Exception e)
+    {
+      // Bad things can happen to good people
+      e.printStackTrace();
+    }
+  }
+}
